a16ab30465eeee8a26057fa39e633bf69542a807
CarolineCF <caroline.cessin-ferrand@le-campus-numerique.fr>
2019-04-02 15:08:55 +0200

    Exo 1 - Les variables

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,13 @@
-//your code here
+
+var kelvin = prompt('Quelle est la température en Kelvin aujourd\'hui ?');
+var celsius = kelvin - 273; // en gros celsius est égal à 294 - 273
+
+var Fahrenheit = celsius * (9/5) + 32;
+
+ Fahrenheit= Math.floor(Fahrenheit)
+
+
+console.log(kelvin+celsius+Fahrenheit);
+
+
 console.log('hello from file');
