19ca193e82f305d0e7c1e3be2ba2404f3114cdfd
CarolineCF <caroline.cessin-ferrand@le-campus-numerique.fr>
2019-04-03 11:34:39 +0200

    Exo-5 les boucles, terminus

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,17 @@
-// you can write js here
+var input = 'Yuraima';
+ input = input.toLowerCase();
+var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+var resultArray = [];
+
+
+for (let i = 0; i < input.length; i++) {
+    
+    let test = vowels.indexOf(input[i]);
+    if (test > - 1) {
+        resultArray.push(input[i]);
+    }
+}
+console.log('la traduction du mot est : ' + resultArray.join('').toUpperCase());
+
 
 console.log('exo-5');
